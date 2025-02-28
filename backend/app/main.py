15882cec0ee076c7116ffbd5aed6d6dd56edda1c
unknown <sophyermiceva@gmail.com>
2025-02-28 10:36:51 +0200

    changed comment

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Real-Time Translation API")
 
-#test git working
+#test git working 3
 @app.get("/")
 async def root():
     return {"message": "API is running"}
